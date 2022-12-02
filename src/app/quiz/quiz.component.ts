165684e456ff4f1948e5a9b92f45ce1f2353c416
Martin Krause <martin.21.krause@gmail.com>
2022-12-02 14:51:35 +0100

    Added quiz service, quiz overview routerlink, style

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from 'src/quiz';
+import { QuizService } from 'src/services/quiz.service';
+
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private quizService: QuizService,
+  ) { }
+
+
+  answersQuestions: Question[] =[];
+  selectedQuestion: Question;
+  id: string
 
   ngOnInit(): void {
+
+  //   this.quizService.getQuizes(this.id).subscribe(response=> {
+  //     console.log(response); 
+  //     this.answersQuestions=response;
+  //     this.selectedQuestion=response[0]
+
+  //     //this.isContentLoaded = true
+  //  });
   }
 
 }
