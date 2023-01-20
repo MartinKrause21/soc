eea96e6c2008251a0b796bb2f0794d3cd6c16065
MartinKrause21 <martin.21.krause@gmail.com>
2023-01-20 15:00:37 +0100

    logout test 3

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
-import {  user, userLogin } from 'src/user';
+import { user, userLogin } from 'src/user';
 import { CookieService } from 'ngx-cookie-service';
 import { Subject } from 'rxjs';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { RegisterVerifyDialogComponent } from 'src/app/register-verify-dialog/register-verify-dialog.component';
 import { FailedLoginDialogComponent } from 'src/app/failed-login-dialog/failed-login-dialog.component';
 import { Router } from '@angular/router';
@@ -18,211 +18,216 @@ export class AuthService {
   userSubject = new Subject<void>();
 
   headers = new Headers();
-  users : user[] = [];
+  users: user[] = [];
   username!: string;
   role: string;
 
   constructor(
     private readonly http: HttpClient,
     private cookies: CookieService,
-    private dialog : MatDialog, 
+    private dialog: MatDialog,
     private router: Router,
   ) { }
 
-      authString = `${this.cookies.get('username')}:${this.cookies.get('password')}`
+  authString = `${this.cookies.get('username')}:${this.cookies.get('password')}`
 
-      headerHttp = new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: 'Basic ' + btoa(this.authString)
-      });
+  headerHttp = new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'Basic ' + btoa(this.authString)
+  });
 
-      getToken(): string {
-        const authString = `${this.cookies.get('username')}:${this.cookies.get('password')}`
-        return 'Basic ' + btoa(authString);
-      }
-
-      getUserRole() {
-        return this.http.get<{role: string}>('https://teach-quiz.herokuapp.com/role',{ headers: this.headerHttp });
-      }
-
-      isLoggedIn(): boolean {
-        return !!(this.cookies.get('username') && this.cookies.get('password'));
-      }
-
-      async login(user: userLogin){
-        let authString = `${user.username}:${user.password}`
-    
-        this.headers.set('Authorization', 'Basic ' + btoa(authString))
-        console.log(authString);
-        
-        try {
-          const response = await fetch('https://teach-quiz.herokuapp.com/login', {
-            method: 'GET',
-            headers: this.headers,
-          });
-          const data_1 = await response.json();
-       
-          //console.log(this.cookies.get('username'));
-          setTimeout(() => {
-            //location.reload()
-            this.router.navigateByUrl("/home").then(() => {
-              location.reload();
-            });
-          },900);
-
-          this.cookies.set('username', user.username);
-          this.cookies.set('password', user.password);
-          
-        }
-         catch (error) {
-          console.log('Error:', error);
-          this.showFailDialog();
-        }
-      }
-
-    //   login(user: userLogin) {
-      
-    //     let authString = `${user.username}:${user.password}`
-    
-    //       this.headers.set('Authorization', 'Basic ' + btoa(authString))
-    //       console.log(authString);
-
-    //     return this.http.get('https://teach-quiz.herokuapp.com/login', {
-    //         params: new HttpParams().set('user', JSON.stringify(user)), 
-    //     }).subscribe(response => {
-    //         if (response) {
-    //             this.cookies.set('username', user.username);
-    //             this.cookies.set('password', user.password);
-          
-    //             setTimeout(() => {
-    //               location.reload()
-    //                   },1200);
-    //         }
-    //     },
-    //     (error: HttpErrorResponse) => {
-    //         console.error('Error: ' + error.message);
-    //         this.showFailDialog();
-    //     });
-    // }
-
-      createUser (user:user) {
-        fetch('https://teach-quiz.herokuapp.com/register', {
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/json',
-         },
-         body: JSON.stringify(user),
-       })
-       .then(() => {
-         console.log('create user Success!');
-         this.cookies.deleteAll();
-         this.showRegisterVerifyialog();
-       })
-       .catch((error) => {
-         console.error('Error:' , error);
-         alert("faileeedddd")
-       });
-   
-     }
-
-     createGuest(username: string, password = "password") 
-     {
-      fetch('https://teach-quiz.herokuapp.com/addUsername/' +username, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-      })
-      .then(() => {
-          this.cookies.set('username', username);
-          this.cookies.set('password',password);
-          console.log('guest Success!');
-      })
-      .catch((error) => {
-          console.error('Error:' , error);
-      });
+  getToken(): string {
+    const authString = `${this.cookies.get('username')}:${this.cookies.get('password')}`
+    return 'Basic ' + btoa(authString);
   }
-   
-  logout() {
+
+  getUserRole() {
+    return this.http.get<{ role: string }>('https://teach-quiz.herokuapp.com/role', { headers: this.headerHttp });
+  }
+
+  isLoggedIn(): boolean {
+    return !!(this.cookies.get('username') && this.cookies.get('password'));
+  }
+
+  async login(user: userLogin) {
+    let authString = `${user.username}:${user.password}`
+
+    this.headers.set('Authorization', 'Basic ' + btoa(authString))
+    console.log(authString);
+
     try {
-       
-        this.cookies.delete('username', '/', 'soc-eight.vercel.app', false, 'Lax');
-        this.cookies.delete('username', '/quiz', 'soc-eight.vercel.app', false, 'Lax');
+      const response = await fetch('https://teach-quiz.herokuapp.com/login', {
+        method: 'GET',
+        headers: this.headers,
+      });
+      const data_1 = await response.json();
 
-        this.cookies.delete('password', '/', 'soc-eight.vercel.app', false, 'Lax');
-        this.cookies.delete('password', '/quiz', 'soc-eight.vercel.app', false, 'Lax');
+      //console.log(this.cookies.get('username'));
+      setTimeout(() => {
+        this.router.navigateByUrl("/home").then(() => {
+          location.reload();
+        });
+      }, 900);
 
-        this.cookies.delete('role', '/', 'soc-eight.vercel.app', false, 'Lax');
-        this.cookies.delete('role', '/quiz', 'soc-eight.vercel.app', false, 'Lax');
-        
-        this.userSubject.next();
-    } catch (error) {
-        console.error('An error occurred while logging out: ', error);
+      
+
+      const expirationDate = new Date();
+      expirationDate.setDate(expirationDate.getDate() + 30); // expires in 30 days
+      this.cookies.set('password', user.password, undefined, 'soc-eight.vercel.app', undefined, false, 'Lax');
+      this.cookies.set('username', user.username, undefined, 'soc-eight.vercel.app', undefined, false, 'Lax');
+
     }
-}
+    catch (error) {
+      console.log('Error:', error);
+      this.showFailDialog();
+    }
+  }
 
-      changePassword( email:string, password : string){
 
-        fetch('https://teach-quiz.herokuapp.com/changePassword/'+ email + "/" + password, {
-          method: 'PUT',
-          headers: new Headers({
-            'Content-Type': "application/json; charset=utf8",
-        }),
-      })
+
+  //   login(user: userLogin) {
+
+  //     let authString = `${user.username}:${user.password}`
+
+  //       this.headers.set('Authorization', 'Basic ' + btoa(authString))
+  //       console.log(authString);
+
+  //     return this.http.get('https://teach-quiz.herokuapp.com/login', {
+  //         params: new HttpParams().set('user', JSON.stringify(user)), 
+  //     }).subscribe(response => {
+  //         if (response) {
+  //             this.cookies.set('username', user.username);
+  //             this.cookies.set('password', user.password);
+
+  //             setTimeout(() => {
+  //               location.reload()
+  //                   },1200);
+  //         }
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //         console.error('Error: ' + error.message);
+  //         this.showFailDialog();
+  //     });
+  // }
+
+  createUser(user: user) {
+    fetch('https://teach-quiz.herokuapp.com/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
       .then(() => {
-        console.log('passw reset Success!');
-        window.location.href="/login"
+        console.log('create user Success!');
+        this.cookies.deleteAll();
+        this.showRegisterVerifyialog();
       })
       .catch((error) => {
-        console.error('Error:' , error);
+        console.error('Error:', error);
         alert("faileeedddd")
       });
 
-    }
+  }
 
-      showFailDialog(): void {
-        this.dialog.open(FailedLoginDialogComponent);
-      }
-
-      showRegisterVerifyialog(): void {
-        this.dialog.open(RegisterVerifyDialogComponent);
-      }
-
-    sendPasswordResetEmail(email: string) {
-
-        fetch('https://teach-quiz.herokuapp.com/changePasswordEmail/' + email, {
-          method: 'GET',
-          headers: new Headers({
-            'Content-Type': "application/json; charset=utf8",
-        }),
+  createGuest(username: string, password = "password") {
+    fetch('https://teach-quiz.herokuapp.com/addUsername/' + username, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => {
+        this.cookies.set('username', username);
+        this.cookies.set('password', password);
+        console.log('guest Success!');
       })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  logout() {
+    try {
+
+      this.cookies.delete('username', '/', 'soc-eight.vercel.app', false, 'Lax');
+      this.cookies.delete('username', '/quiz', 'soc-eight.vercel.app', false, 'Lax');
+
+      this.cookies.delete('password', '/', 'soc-eight.vercel.app', false, 'Lax');
+      this.cookies.delete('password', '/quiz', 'soc-eight.vercel.app', false, 'Lax');
+
+      this.cookies.delete('role', '/', 'soc-eight.vercel.app', false, 'Lax');
+      this.cookies.delete('role', '/quiz', 'soc-eight.vercel.app', false, 'Lax');
+
+      this.userSubject.next();
+      location.reload();
+    } catch (error) {
+      console.error('An error occurred while logging out: ', error);
+    }
+  }
+
+  changePassword(email: string, password: string) {
+
+    fetch('https://teach-quiz.herokuapp.com/changePassword/' + email + "/" + password, {
+      method: 'PUT',
+      headers: new Headers({
+        'Content-Type': "application/json; charset=utf8",
+      }),
+    })
+      .then(() => {
+        console.log('passw reset Success!');
+        window.location.href = "/login"
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert("faileeedddd")
+      });
+
+  }
+
+  showFailDialog(): void {
+    this.dialog.open(FailedLoginDialogComponent);
+  }
+
+  showRegisterVerifyialog(): void {
+    this.dialog.open(RegisterVerifyDialogComponent);
+  }
+
+  sendPasswordResetEmail(email: string) {
+
+    fetch('https://teach-quiz.herokuapp.com/changePasswordEmail/' + email, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': "application/json; charset=utf8",
+      }),
+    })
       .then(() => {
         console.log('passw email send Success!');
       })
       .catch((error) => {
-        console.error('Error:' , error);
+        console.error('Error:', error);
         alert("faileeedddd")
       });
-  
-    } 
+
+  }
 
 
-    verifyUser(code: string) {
+  verifyUser(code: string) {
 
-      fetch('https://teach-quiz.herokuapp.com/verify/' + code , {
-        method: 'GET',
-        headers: new Headers({
-          'Content-Type': "application/json; charset=utf8",
+    fetch('https://teach-quiz.herokuapp.com/verify/' + code, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': "application/json; charset=utf8",
       }),
     })
-    .then(() => {
-      console.log('verify Success!');
-    })
-    .catch((error) => {
-      console.error('Error:' , error);
+      .then(() => {
+        console.log('verify Success!');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
 
-      alert("faileeedddd")
-    });
+        alert("faileeedddd")
+      });
 
   }
 
