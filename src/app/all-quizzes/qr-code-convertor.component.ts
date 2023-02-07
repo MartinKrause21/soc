3fef83809e5920e801a7ba668b14dfb3c1c46c22
Martin Krause <martin.21.krause@gmail.com>
2023-02-07 17:54:45 +0100

    Added animations for al quizzes, edited message for 0 quiz length

--- src/app/all-quizzes/qr-code-convertor.component.ts
+++ src/app/all-quizzes/qr-code-convertor.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { allUserQuizes } from 'src/quiz';
 import { QuizService } from 'src/services/quiz.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-qr-code-convertor',
   templateUrl: './qr-code-convertor.component.html',
-  styleUrls: ['./qr-code-convertor.component.css']
+  styleUrls: ['./qr-code-convertor.component.css'],
+  animations: [
+    trigger('animateBottom', [
+      state('hidden', style({
+        opacity: 0,
+        transform: 'translateY(10%)',
+        filter: 'blur(3px)'
+      })),
+      transition('hidden => visible', animate('300ms ease-in'))
+    ])
+  ]
 })
 export class QrCodeConvertorComponent implements OnInit {
 
@@ -14,9 +25,9 @@ export class QrCodeConvertorComponent implements OnInit {
 
   constructor(
     private quizService : QuizService,
-  ) {    this.originalQuizzes = this.quizzes;}
+  ) { this.originalQuizzes = this.quizzes;}
 
-
+  visibility = 'hidden';
   contentLoaded = true;
 
   public QRcodeText:string;
@@ -29,6 +40,10 @@ export class QrCodeConvertorComponent implements OnInit {
       this.contentLoaded = false;
       
     });
+
+    setTimeout(() => {
+      this.visibility = 'visible';
+    }, 300);
   }
 
   searchTerm: string;
