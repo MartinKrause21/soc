652eff3be01ebab16c9f25c863a243bcc3fb8811
Martin Krause <martin.21.krause@gmail.com>
2023-02-19 12:51:10 +0100

    Set favourite method fixed link

--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -187,7 +187,7 @@ export class QuizService {
 
     let authString = `${this.cookies.get("username")}:${this.cookies.get("password")}`
 
-    fetch(`https://teach-quiz.herokuapp.com/save/score/${quizName}`, {  
+    fetch(`https://teach-quiz.herokuapp.com/set/favourite/${quizName}`, {  
       method: 'PUT',
       headers: new Headers({
       'Authorization': 'Basic '+btoa(authString), 
